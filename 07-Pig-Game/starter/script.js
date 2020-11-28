90a5c7962c65aee318e79a8d431ec2ee833203e6
yashusunny <53004258+yashusunny@users.noreply.github.com>
2020-11-28 10:43:26 +0530

    Pig Game update

---
+++ 07-Pig-Game/starter/script.js
@@ -0,0 +1,22 @@
+'use strict';
+// Selecting elements
+const score0EL = document.querySelector('#score--0');
+const score1EL = document.getElementById('score--1');
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+score0EL.textContent = 0;
+score1EL.textContent = 0;
+diceEl.classList.add('hidden');
+
+//Rolling the dice
+
+// document.querySelector('.btn--roll').addEventListener('click');
+
+btnRoll.addEventListener('click', function () {
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png;`;
+});
